ec606d71213d1c23048ae4b0a47d0eb4d823f1b1
S0PHI3K <sophiercowley@hotmail.com>
2022-12-14 09:26:52 +0000

    first attempt at descendingOrder

--- challenges/descendingOrder/descendingOrder.js
+++ challenges/descendingOrder/descendingOrder.js
@@ -2,18 +2,16 @@
 // and return it with its digits in descending order. Essentially, rearrange the digits
 // to create the highest possible number.
 
-//Examples:
-//Input: 42145 Output: 54421
-//Input: 145263 Output: 654321
-//Input: 123456789 Output: 987654321
 
 function descendingOrder(n){
-  let order = []
-  //for loop to iterate through the numbers
-  //num 1 = (n[0])
-  //num 2 = (n[1])
-  //if num 1 is higher than num 2, move it up the index or push it into the array, else push 2 into the array?
-  //then return the array.join('')
+  const myFunc = num => Number(num)
+  const intArr = Array.from(String(n), myFunc)
+  // use the above to lines from the internet, but not fully sure how they work. 
+  
+  const highToLow = intArr.sort().reverse()
+  const output = highToLow.join('')
+  const result = parseInt(output)
+  return result
 
   }
 
