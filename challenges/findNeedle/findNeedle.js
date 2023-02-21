af2d1ec4321229600b00afaac1330d18db005380
S0PHI3K <sophiercowley@hotmail.com>
2023-02-21 15:30:53 +0000

    updating needle test

--- challenges/findNeedle/findNeedle.js
+++ challenges/findNeedle/findNeedle.js
@@ -5,16 +5,12 @@
 // Example(Input --> Output)
 // ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5"
 
-function findNeedle(haystack) {
+  function findNeedle(haystack) {
   
     for(let i = 0; i < haystack.length; i++){
       if (haystack[i] === "needle"){
-        "found the needle at position " + i
-      } else {
-        "Your function didn't return anything."
-      }
+        return "found the needle at position " + i;
+      } 
     }
   }
-
-findNeedle
 module.exports = findNeedle
