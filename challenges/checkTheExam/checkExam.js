0f03be7e7af53c08ae281d70c7ed9a4b379c054b
Sophie <sophiercowley@hotmail.com>
2024-05-19 19:13:15 +0100

    check the exam 7kyu

---
+++ challenges/checkTheExam/checkExam.js
@@ -0,0 +1,36 @@
+// The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+// The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+// If the score < 0, return 0.
+
+// For example:
+
+//     Correct answer    |    Student's answer   |   Result         
+//  ---------------------|-----------------------|-----------
+//  ["a", "a", "b", "b"]   ["a", "c", "b", "d"]  →     6
+//  ["a", "a", "c", "b"]   ["a", "a", "b", "" ]  →     7
+//  ["a", "a", "b", "c"]   ["a", "a", "b", "c"]  →     16
+//  ["b", "c", "b", "a"]   ["" , "a", "a", "c"]  →     0
+
+function checkExam(array1, array2) {
+  
+    let count = 0
+    
+    for (let i = 0; i < array1.length; i++) {
+  
+        if (array1[i] === array2[i]) {
+          count += 4
+        } else if (array2[i] === ""){
+          count += 0
+        } else {
+          count -= 1
+        }
+      }
+            
+    return count >= 0 ? count : 0;
+
+    
+    // Alternative solutions using the reduce method. 
+
+    checkExam = (x, y) => (x = y.reduce((s, e, i) => s + (e === x[i] ? 4 : e === '' ? 0 : -1), 0)) > 0 ? x : 0;
